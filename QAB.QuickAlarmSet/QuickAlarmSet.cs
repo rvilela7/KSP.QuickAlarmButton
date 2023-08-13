b47b8136e6ecd7744ca8d866e23928271428b580
rvilela7 <ruivil@gmail.com>
2023-08-13 20:09:19 +0100

    .

--- QAB.QuickAlarmSet/QuickAlarmSet.cs
+++ QAB.QuickAlarmSet/QuickAlarmSet.cs
@@ -1,13 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using KSP.UI.Screens;
 using UnityEngine;
 
-namespace QAB.QuickAlarmSet
+namespace YourModNamespace
 {
-    public class QuickAlarmSet : MonoBehaviour
+    [KSPAddon(KSPAddon.Startup.MainMenu, true)]
+    public class WaypointAlarmModule : MonoBehaviour
     {
+        private static ApplicationLauncherButton toolbarButton;
+        private static Waypoint currentWaypoint;
+
+        private void Start()
+        {
+            GameEvents.onGUIApplicationLauncherReady.Add(OnGUIApplicationLauncherReady);
+            GameEvents.onGUIApplicationLauncherUnreadifying.Add(OnGUIApplicationLauncherUnreadifying);
+        }
+
+        private void OnGUIApplicationLauncherReady()
+        {
+            if (ApplicationLauncher.Ready)
+            {
+                Texture2D buttonTexture = GameDatabase.Instance.GetTexture("QAB/Textures/ToolbarIcon", false);
+                toolbarButton = ApplicationLauncher.Instance.AddModApplication(
+                    OnToolbarButtonToggleOn,
+                    OnToolbarButtonToggleOff,
+                    null,
+                    null,
+                    null,
+                    null,
+                    ApplicationLauncher.AppScenes.VAB | ApplicationLauncher.AppScenes.SPH,
+                    buttonTexture);
+            }
+        }
+
+        private void OnGUIApplicationLauncherUnreadifying(GameScenes scene)
+        {
+            if (toolbarButton != null)
+            {
+                ApplicationLauncher.Instance.RemoveModApplication(toolbarButton);
+            }
+        }
+
+        private void OnToolbarButtonToggleOn()
+        {
+            currentWaypoint = FlightGlobals.ActiveVessel?.targetObject as Waypoint;
+            if (currentWaypoint != null)
+            {
+                // Calculate the time until the waypoint and add an alarm notification
+                double timeToWaypoint = currentWaypoint.UT - Planetarium.GetUniversalTime();
+                AlarmClockScenario.AddAlarm(
+                    new KACWrapper.KACAPI.AlarmObject(FlightGlobals.ActiveVessel.vesselName, timeToWaypoint, "Waypoint Alarm"));
+            }
+        }
+
+        private void OnToolbarButtonToggleOff()
+        {
+            // Remove the notification for the current waypoint
+            if (currentWaypoint != null)
+            {
+                AlarmClockScenario.DeleteAlarm(currentWaypoint.UT, FlightGlobals.ActiveVessel.vesselName);
+            }
+        }
     }
 }
